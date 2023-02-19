e08a687a5620a1ad03b6b36998a14a6aec2714f6
Ricky Gunawan <gunawanricky94@gmail.com>
2023-02-19 23:30:01 +0700

    final

--- src/service-worker-pwa.js
+++ src/service-worker-pwa.js
@@ -1,21 +1,18 @@
 import {precacheAndRoute} from 'workbox-precaching';
-// Testing add something into service worker
-console.log("add something");
 
 let click_open_url;
 self.addEventListener("push", function(event){
-     let push_message = event.data.text();
-
+    //  let push_message = event.data.text();
     click_open_url = "https://google.com";
      const options = {
-        body: push_message.body,
+        body: "You received a push message",
         icon: "./img/icons/android-chrome-192x192.png",
         image: "./img/icons/android-chrome-512x512.png",
         vibrate: [200, 100, 200, 100, 200, 100, 200],
         tag: "vibration-sample"
      };
      event.waitUntil(
-        self.registration.showNotification("My Notification", options)
+        self.registration.showNotification("Notification Test", options)
      );
 });
 
