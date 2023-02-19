b5c985fb12dbc27ad7d1458a8a612f0192b8ddf1
Ricky Gunawan <gunawanricky94@gmail.com>
2023-02-19 20:21:31 +0700

    revert

--- src/service-worker-pwa.js
+++ src/service-worker-pwa.js
@@ -2,9 +2,11 @@ import {precacheAndRoute} from 'workbox-precaching';
 // Testing add something into service worker
 console.log("add something");
 
+let click_open_url;
 self.addEventListener("push", function(event){
      let push_message = event.data.text();
 
+    click_open_url = "https://google.com";
      const options = {
         body: push_message.body,
         icon: "./img/icons/android-chrome-192x192.png",
@@ -21,7 +23,7 @@ self.addEventListener("notificationclick", function(event){
     const clickedNotification = event.notification;
     clickedNotification.close();
     if(click_open_url) {
-        const promiseChain = clients.openWindow("https://google.com");
+        const promiseChain = clients.openWindow(click_open_url);
         event.waitUntil(promiseChain);
     }
 });
